a923a912ed25a4b0b5608797f70fac2d521f95fd
PhilCodesNow <philipmayojr@outlook.com>
2021-02-02 04:43:17 -0500

    got list input and output working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import Input from './Input';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Output from './Output';
 import './App.css';
 
@@ -10,11 +10,25 @@ import './App.css';
 function App() {
 
   const [list, setList] = useState(['list 1', 'list 2'])
- 
+  let listRef = useRef()
+
+
+
+  function handleInputSubmit(){
+    const newList = listRef.current.value
+    setList(prevList => {
+      return [...prevList, newList]
+    })
+    listRef.current.value = ''
+  }
+
+
   console.log(list)
   return (
     <div>
       <Input
+      handleInputSubmit={handleInputSubmit}
+      listRef={listRef}
       />
       <Output
       list={list}/>
--- src/Input.js
+++ src/Input.js
@@ -2,13 +2,20 @@ import React from 'react';
 import AddBoxIcon from '@material-ui/icons/AddBox';
 import './Input.css';
 
-function Input() {
+function Input(props) {
+    const {
+        handleInputSubmit,
+        listRef
+    } = props
+
+
+
 
     return (
         <div className="input">
             <form>
-                <input type="text" placeholder="New Item"/>
-                <AddBoxIcon></AddBoxIcon>
+                <input type="text" placeholder="New Item" ref={listRef}/>
+                <AddBoxIcon onClick={handleInputSubmit}></AddBoxIcon>
             </form>
         </div>
     )
